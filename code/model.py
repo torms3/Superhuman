9183de27fd369114e5dda092138e747b56d9acb2
torms3 <kisuklee@mit.edu>
2018-01-20 23:07:53 -0500

    parallel eval

--- code/model.py
+++ code/model.py
@@ -9,69 +9,38 @@ import loss
 from rsunet import RSUNet
 
 
-class Model(object):
-
+class TrainModel(RSUNet):
+    """
+    RSUNet for training.
+    """
     def __init__(self, opt):
-        # Options.
-        self.opt = opt
-
-        # In/out spec.
+        super(TrainModel, self).__init__(opt.in_spec, opt.out_spec, opt.depth)
         self.in_spec = opt.in_spec
         self.out_spec = opt.out_spec
-
-        # Create a net.
-        net = RSUNet(opt.in_spec, opt.out_spec, opt.depth)
-        if opt.batch_size > 1:
-            net = torch.nn.DataParallel(net)
-        self.net = net.cuda()
-
-        # Create an optimizer and a loss function.
-        self.optimizer = torch.optim.Adam(net.parameters(), lr=opt.base_lr)
         self.loss_fn = loss.BinaryCrossEntropyWithLogits()
 
-    def step(self, i, sample):
-        t0 = time.time()
-        sample = self.make_variables(sample)
-        t1 = time.time()
-        preds = self.net(*[sample[k] for k in self.in_spec])
-        losses, nmsks = self.eval_loss(preds, sample)
-        self.update_model(losses)
-        t2 = time.time()
-        # self.log_errors(losses, nmsks)
-        # avg_loss = dict([(k, v.data[0]/nmsks[k]) for k,v in losses.items()])
-        avg_loss = sum(losses.values())/sum(nmsks.values())
-        disp  = "Iter %6d: " % (i+1)
-        disp += "loss = %.3f " % avg_loss
-        disp += "(frontend = %.3f s, backend = %.3f s, elapsed = %.3f s)" % (t1-t0, t2-t1, t2-t0)
-        print(disp)
+    def forward(self, sample):
+        """Runs forward pass and computes loss.
 
-    def make_variables(self, sample):
-        # Inputs.
-        for k in self.in_spec:
-            sample[k] = Variable(sample[k], requires_grad=True).cuda()
-        # Labels.
-        for k in self.out_spec:
-            sample[k] = Variable(sample[k], requires_grad=False).cuda(async=True)
-        # Masks.
-        for k in self.out_spec:
-            k +='_mask'
-            assert k in sample
-            sample[k] = Variable(sample[k], requires_grad=False).cuda(async=True)
-        return sample
+        Args:
+            sample (dictionary): Containing inputs, labels, and masks.
+
+        Returns:
+            Variable:
+                Computed loss.
+        """
+        # Forward pass.
+        inputs = [sample[k] for k in self.in_spec]
+        preds = super(TrainModel, self).forward(*inputs)
+        # Evaluates loss.
+        return self.eval_loss(preds, sample)
 
     def eval_loss(self, preds, sample):
-        losses, nmasks = dict(), dict()
-        for k in self.out_spec.keys():
-            assert k+'_mask' in sample
-            mask = sample[k+'_mask']
-            losses[k] = self.loss_fn(preds[k], sample[k], mask=mask)
-            nmasks[k] = mask.sum()
-        return losses, nmasks
-
-    def update_model(self, losses):
-        self.optimizer.zero_grad()
-        total_loss = sum(losses.values())
-        total_loss.backward()
-        self.optimizer.step()
-
-    # def log_errors(self, losses, nmasks):
+        self.loss = dict()
+        self.nmsk = dict()
+        for i, k in enumerate(self.out_spec):
+            label = sample[k]
+            mask  = sample[k + '_mask']
+            self.loss[k] = self.loss_fn(preds[i], label, mask)
+            self.nmsk[k] = mask.sum()
+        return sum(self.loss.values())
--- code/train.py
+++ code/train.py
@@ -9,8 +9,7 @@ from torch.autograd import Variable
 from torch.utils.data import DataLoader
 
 from dataset import SNEMI3D_Dataset
-import loss
-from model import Model
+import model
 from options import BaseOptions
 from sampler import get_sampler
 
@@ -19,8 +18,8 @@ def train(opt):
     """
     Training.
     """
-    # Create a net.
-    net = model.RSUNet(opt.in_spec, opt.out_spec, opt.depth)
+    # Create a model.
+    net = model.TrainModel(opt)
     if opt.batch_size > 1:
         net = torch.nn.DataParallel(net)
     net = net.cuda()
@@ -36,9 +35,8 @@ def train(opt):
                         num_workers=opt.num_workers,
                         pin_memory=True)
 
-    # Create an optimizer and a loss function.
-    # optimizer = torch.optim.Adam(net.parameters(), lr=opt.base_lr)
-    # loss_fn = loss.BinaryCrossEntropyWithLogits()
+    # Create an optimizer.
+    optimizer = torch.optim.Adam(net.parameters(), lr=opt.base_lr)
 
     # Profiling.
     fend = list()
@@ -47,16 +45,14 @@ def train(opt):
     start = time.time()
     print("======= BEGIN TRAINING LOOP ========")
     for i, sample in enumerate(dataloader):
-        # inputs, labels, masks = make_variables(sample, opt)
+        sample = make_variables(sample, opt)
 
-        # Step.
-        # backend = time.time()
-        # preds = net(*inputs)
-        # losses, nmsks = eval_loss(opt.out_spec, preds, labels, masks, loss_fn)
-        # update_model(optimizer, losses)
-        # backend = time.time() - backend
-
-        model.step(i, sample)
+        backend = time.time()
+        optimizer.zero_grad()
+        loss = net(sample).sum()
+        loss.backward()
+        optimizer.step()
+        backend = time.time() - backend
 
         # Elapsed time.
         elapsed  = time.time() - start
@@ -64,53 +60,26 @@ def train(opt):
         bend.append(backend)
 
         # Display.
-        avg_loss = sum(losses.values())/sum(nmsks.values())
-        print("Iter %6d: loss = %.3f (frontend = %.3f s, backend = %.3f s, elapsed = %.3f s)" % (i+1, avg_loss, fend[i], bend[i], elapsed))
+        print("Iter %6d: loss = %.3f (frontend = %.3f s, backend = %.3f s, elapsed = %.3f s)" % (i+1, loss, fend[i], bend[i], elapsed))
         start = time.time()
 
     n = opt.max_iter - 10
     print("n = %d, frontend = %.3f s, backend = %.3f s" % (n, sum(fend[-n:])/n, sum(bend[-n:])/n))
 
 
-def update_model(optimizer, losses):
-    """Runs the backward pass and updates model parameters."""
-    optimizer.zero_grad()
-    total_loss = sum(losses.values())
-    total_loss.backward()
-    optimizer.step()
-
-
-def eval_loss(out_spec, preds, labels, masks, loss_fn):
-    """
-    Evaluates the error of the predictions according to the available
-    labels and masks.
-
-    Assumes labels are ordered according to the sample_spec.
-    """
-    assert len(masks) == len(labels), "Mismatched masks and labels"
-    assert len(preds) == len(labels), "Mismatched preds and labels"
-
-    losses = dict()
-    nmasks = dict()
-
-    for i, k in enumerate(out_spec.keys()):
-        losses[k] = loss_fn(preds[i], labels[i], masks[i])
-        nmasks[k] = masks[i].sum()
-
-    return losses, nmasks
-
-
 def make_variables(sample, opt):
-    """Creates the Torch variables for a sample."""
-    inputs = opt.in_spec.keys()
-    labels = opt.out_spec.keys()
-    masks  = [l + '_mask' for l in labels]
-
-    input_vars = [Variable(sample[k], requires_grad=True).cuda() for k in inputs]
-    label_vars = [Variable(sample[k], requires_grad=False).cuda(async=True) for k in labels]
-    mask_vars  = [Variable(sample[k], requires_grad=False).cuda(async=True) for k in masks]
-
-    return input_vars, label_vars, mask_vars
+    # Inputs.
+    for k in opt.in_spec:
+        sample[k] = Variable(sample[k], requires_grad=True).cuda()
+    # Labels.
+    for k in opt.out_spec:
+        sample[k] = Variable(sample[k], requires_grad=False).cuda(async=True)
+    # Masks.
+    for k in opt.out_spec:
+        k +='_mask'
+        assert k in sample
+        sample[k] = Variable(sample[k], requires_grad=False).cuda(async=True)
+    return sample
 
 
 if __name__ == "__main__":
