d3636208890ca2d65bcdcc1d239cebaa111d6595
Kisuk Lee <kisuklee@mit.edu>
2018-02-01 03:15:59 -0500

    update

--- code/options.py
+++ code/options.py
@@ -33,7 +33,7 @@ class BaseOptions(object):
         # Model spec.
         self.parser.add_argument('--fov', type=int, default=[32,160,160], nargs='+')
         self.parser.add_argument('--depth', type=int, default=4)
-        self.parser.add_argument('--momentum', type=float, default=0.001)
+        self.parser.add_argument('--momentum', type=float, default=0.1)
 
         # Data augmentation & transform.
         self.parser.add_argument('--long_range', action='store_true')
